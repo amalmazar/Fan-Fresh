d6166c5acb7112e89d601e236f5be68230e7a816
amalmazar <amalmazar@gmail.com>
2017-11-30 21:32:05 -0500

    project updates

---
+++ FanFresh/controllers/fanFreshController.js
@@ -0,0 +1,20 @@
+//    We need to get this working.
+
+//    var express = require("express");
+//
+//    var router = express.Router();
+//
+//    // Basic route that sends the user to the index page
+//    router.get("/", function(req, res) {
+//        res.render("index");
+//    });
+//
+//    // Route that sends the user to the top artists page
+//    router.get("/top", function(req, res) {
+//      res.render("top");
+//    });
+//
+//    // Route that sends the user to the main search page (this is where they will go after they log in)
+//    router.get("/home", function(req, res) {
+//      res.render("home");
+//    });
--- FanFresh/public/js/script.js
+++ FanFresh/public/js/script.js
@@ -23,7 +23,7 @@ function createFanPageTop() {
 function getArtist(whichInput) {
     // Constructing a queryURL
     var artist = $(whichInput).val().trim();
-    console.log(artist);
+    // Console.log(artist);
     
     var queryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artist + "&api_key=3ff3b259d85abca5d13695ff68bfbe51&format=json";
     
@@ -34,8 +34,8 @@ function getArtist(whichInput) {
     })
     // After data comes back from the request
     .done(function(response) {
-      console.log(response);
-      console.log(queryURL);
+    //  console.log(response);
+    //  console.log(queryURL);
       populatePhoto("<img src=" + response.artist.image[4]['#text'] +">");
       populateName(response.artist.name);
       populateBio(response.artist.bio.content);
@@ -51,6 +51,18 @@ function populateName(artistname) {
 function populateBio(bio) {
     $("#artistBio").html(bio);
 };
+
+// This function just toggles the color of the favorites heart icon 
+$('#favorite').click(function() {
+    if ($(this).find('i').html('favorite_border')) {
+        $(this).find('i').html('favorite');
+        $(this).find('i').css("color", "#ff404e");
+    } else {
+        $(this).find('i').html('favorite_border');
+        $(this).find('i').css("color", "#fff");
+    }
+});
+
 
 
 createFanPageMain();
